531667c03665547b04d4d8292f5bf540476e4e32
a035 <marlon@erler.cc>
2022-11-20 20:20:16 +0100

    added basic cli

--- index.ts
+++ index.ts
@@ -1,94 +1,129 @@
 #! /usr/bin/env node
 
 //IMPORTS
+import Cp from "child_process";
 import Fs from "fs/promises";
 import Path from "path";
 
 //UTILITY
 async function ask(question: string): Promise<string> {
 	return new Promise(res => {
-		console.log(question);
+		process.stdout.write(question);
 		process.stdin.once("data", data => {
 			res(data.toString());
 		});
 	});
 }
 
-//FILE SYSTEM
-//create nessecary folders
-let folders_to_create = [
-	"../Modules",
+//MAIN
+async function startup() {
+	//FILE SYSTEM
+	//create nessecary folders
+	let folders_to_create = [
+		"../Modules",
 
-	"Base",
+		"Base",
 
-	"Frontend",
-	"Frontend/Applications",
-	"Frontend/Start",
-	"Frontend/Themes",
-];
+		"Frontend",
+		"Frontend/Applications",
+		"Frontend/Start",
+		"Frontend/Themes",
+	];
 
-let i = 0;
-while (i < folders_to_create.length) {
-	console.log("Checking folder %i of %i...", i + 1, folders_to_create.length);
-	let path = Path.join("Modules", folders_to_create[i]);
+	let i = 0;
+	while (i < folders_to_create.length) {
+		console.log("Checking folder %i of %i...", i + 1, folders_to_create.length);
+		let path = Path.join("Modules", folders_to_create[i]);
 
-	try {
-		await Fs.mkdir(path);
-		console.log("OK:\tCreated folder '%s'.", path);
+		try {
+			await Fs.mkdir(path);
+			console.log("OK:\tCreated folder '%s'.", path);
+		}
+		catch {
+			console.log("WARN:\tDid not create '%s'.", path);
+		}
+
+		try {
+			await Fs.stat(path);
+			console.log("OK:\t'%s' exists.", path);
+		} catch {
+			console.error("ERR:\t'%s' does not exist.", path);
+			process.exit();
+		}
+
+		i++;
 	}
-	catch {
-		console.log("WARN:\tDid not create '%s'.", path);
-	}
+	console.log("");
 
+	//CONFIGURATION
+	let configuration_path = "Modules/Base/config.json";
+	let configuration: { [field: string]: any } = {};
+	let configuration_default: typeof configuration = {
+		name: {
+			en: "Untitled Station",
+		},
+		greeting: {
+			en: "Welcome!",
+		},
+		number: 1,
+
+		commands: {},
+		groups: {},
+		entry_permits: [],
+	};
+
+	//get configuration
 	try {
-		await Fs.stat(path);
-		console.log("OK:\t'%s' exists.", path);
-	} catch {
-		console.error("ERR:\t'%s' does not exist.", path);
-		process.exit();
-	}
+		let configuration_text = await Fs.readFile(configuration_path, {encoding: "utf8"});
 
-	i++;
-}
-console.log("");
+		try {
+			configuration = JSON.parse(configuration_text);
 
-//CONFIGURATION
-let configuration_path = "Modules/Base/config.json";
-let configuration: { [field: string]: any } = {};
-let configuration_default: typeof configuration = {
-	name: {
-		en: "Untitled Station",
-	},
-	greeting: {
-		en: "Welcome!",
-	},
-	number: 1,
-
-	commands: {},
-	groups: {},
-	entry_permits: [],
-};
-
-//get configuration
-try {
-	let configuration_text = await Fs.readFile(configuration_path, {encoding: "utf8"});
-
-	try {
-		configuration = JSON.parse(configuration_text);
-
-		//make sure number does not contain a zero
-		if (configuration.number.toString().split("").includes("0")) {;
-			console.error("ERR:\tStation number cannot contain a zero.");
-			process.exit()
+			//make sure number does not contain a zero
+			if (configuration.number.toString().split("").includes("0")) {;
+				console.error("ERR:\tStation number cannot contain a zero.");
+				process.exit()
+			}
+		} catch {
+			console.log("ERR:\tFailed to parse configuration file.");
+			process.exit();
 		}
 	} catch {
-		console.log("ERR:\tFailed to parse configuration file.");
+		await Fs.writeFile(configuration_path, JSON.stringify(configuration_default, null, 4));
+		console.log("Created configuration file at '%s'. \nWARN\tPlease edit the configuration file before using this station.", configuration_path);
 		process.exit();
 	}
-} catch {
-	await Fs.writeFile(configuration_path, JSON.stringify(configuration_default, null, 4));
-	console.log("Created configuration file at '%s'. \nWARN\tPlease edit the configuration file before using this station.", configuration_path);
-	process.exit();
+
+	console.log(configuration.greeting.en);
+
+	startCLI();
 }
 
-console.log(configuration.greeting.en);
+function spawn(command: string, write: (output: string) => void) {
+	let cwd: string = process.cwd();
+	let cp = Cp.spawn(command, [], { shell: true, cwd: cwd});
+
+	cp.stdout.on("data", data => {
+		write(data);
+	});
+	cp.on("exit", () => {
+		write("EXITED");
+	});
+}
+
+async function startCLI() {
+	let command = await ask("> ");
+	spawn(command, data => {
+		switch (data) {
+			case "EXITED": {
+				return startCLI();
+			}
+			default: {
+				console.log(data.toString());
+			}
+		}
+	});
+}
+
+//STARTUP
+await startup();
